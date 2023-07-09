85c0e3441db2777ff494c27f0aaeedc43b050d5f
Dean Lawrence <djlawren@umich.edu>
2023-07-08 21:55:29 -0400

    Added ability to query liked songs

--- frontend/src/Components/SpotifyInterface.js
+++ frontend/src/Components/SpotifyInterface.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 
 const SpotifyInterface = ({ activePlaylist, setPlaylists, setTracks }) => {
     const CLIENT_ID = 'b2363da7847344cfa904d18c67075f76';
-    const REDIRECT_URI = 'http://algorhythms-frontend.s3-website-us-east-1.amazonaws.com/';
+    const REDIRECT_URI = 'http://localhost:3000';
     const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
     const RESPONSE_TYPE = 'token';
 
@@ -49,21 +49,31 @@ const SpotifyInterface = ({ activePlaylist, setPlaylists, setTracks }) => {
                 
             }
         }).then((response) => {
-            setPlaylists(response.data.items);
+            var playlists = response.data.items;
+            playlists.unshift({
+                id: 'liked',
+                name: 'Liked Songs'
+            });
+
+            setPlaylists(playlists);
         }).catch((err) => console.log(err));
     };
 
     const retrieveActivePlaylist = () => {
-        if (activePlaylist == "") {
-            return
+        var queryEndpoint = '';
+        if (activePlaylist === '') {
+            return;
+        } else if (activePlaylist === 'liked') {
+            queryEndpoint = `https://api.spotify.com/v1/me/tracks`;
+        } else {
+            queryEndpoint = `https://api.spotify.com/v1/playlists/${activePlaylist}/tracks`;
         }
 
-        axios.get(`https://api.spotify.com/v1/playlists/${activePlaylist}/tracks`, {
+        axios.get(queryEndpoint, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         }).then((response) => {
-            console.log(response.data.items);
             setTracks(response.data.items);
         }).catch((err) => console.log(err));
     };
@@ -71,7 +81,7 @@ const SpotifyInterface = ({ activePlaylist, setPlaylists, setTracks }) => {
     return (
         <div style={{marginTop: '30px'}}>
             {!token ?
-                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-library-read`}>Login
                     to Spotify</a>
                 : <Button variant='secondary' onClick={logout}>Logout</Button>}
         </div>
