8c3bcde609930772df9ff8455e9c9b79e183eba8
anilmenon14 <anil.menon14@gmail.com>
2023-07-29 04:13:09 -0400

    Shell scripting to train models on Slurm resource managed clusters

---
+++ models/scripts/shell_scripts/batch_gcnconv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=pytorch-geometric-data-gen
+#SBATCH --account=siads699s23_class        # change to your account
+#SBATCH --partition=largemem
+#SBATCH --nodes=1                # node count, change as needed
+#SBATCH --ntasks-per-node=1      # do not change, leave as 1 task per node
+#SBATCH --cpus-per-task=1       # cpu-cores per task, change as needed
+#SBATCH --mem=1000g               # memory per node, change as needed
+#SBATCH --time=05:00:00
+#SBATCH --mail-type=NONE
+
+# These modules are required. You may need to customize the module version
+# depending on which cluster you are on.
+module load python/3.10.4 pyarrow/8.0.0
+
+# Change later by importing this from another file
+source /home/anilcm/myenv/bin/activate
+
+# Pass the folder name within 'saved_files/data_representations' that the model will read data from
+data_version_input="$1"
+
+echo ${SLURM_JOB_ID}
+
+python -u ../graph_network_GCNConv_mse_loss.py "$data_version_input" >> slurm-${SLURM_JOB_ID}.out 2>&1
